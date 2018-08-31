2fb32bbffe567fbc1d1decbd72b53ddeff17f762
Alfred Loo <alfred97620@gmail.com>
2018-08-31 16:03:59 +0800

    add template

---
+++ template/template.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require('partials/html-head.php'); ?>
+</head>
+<body>
+  <!-- header -->
+  <?php require('partials/header.php'); ?>
+
+  <!-- body -->
+  
+
+  <!-- footer -->
+  <?php require('partials/footer.php'); ?>
+</body>
+</html>
