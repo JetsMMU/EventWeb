46d401ccbc143e5c4ee7779ee8759f11d73ef105
Alfred Loo <alfred97620@gmail.com>
2018-08-31 23:20:25 +0800

    update template, add comments

--- template/template.php
+++ template/template.php
@@ -1,3 +1,27 @@
+<?php
+  session_start();
+  
+  // Redirect guest to login page
+  /**
+   * Use these lines only if the file does not allow guest(non-registered user) to access  
+   */
+	// if (!isset($_SESSION['username'])) {
+	// 	header("location: login.php");
+	// }
+
+  // Connect to database
+  /**
+   * Use these lines only if the file requires database connection. 
+   */
+	// $config = require('config.php');
+	// $dsn = $config['connection'] . ';dbname=' . $config['dbname'] . ';charset=' . $config['charset'];
+	// try {
+	// 	$pdo = new PDO($dsn, $config['username'], $config['password'], $config['options']);
+	// } catch (PDOException $e) {
+	// 	die($e->getMessage());
+	// }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
