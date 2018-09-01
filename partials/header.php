ba94f32c440c23bf7f539c884b792fdaf5695b3b
Alfred Loo <alfred97620@gmail.com>
2018-09-01 13:31:04 +0800

    update partials, change a tag

--- partials/header.php
+++ partials/header.php
@@ -7,17 +7,17 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+        <li class="nav-item">
+          <a class="nav-link" href="/home.php">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Browse Events</a>
+          <a class="nav-link" href="/event.php">Browse Events</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Sign Up</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
+          <a class="nav-link" href="/login.php">Login</a>
         </li>
         <!-- To add a new file to header -->
         <!-- <li class="nav-item">
