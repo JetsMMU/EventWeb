3431e345e47202363d31b6f91c04cee16830b669
Alfred Loo <alfred97620@gmail.com>
2018-09-04 01:13:47 +0800

    add browse event

---
+++ event.php
@@ -0,0 +1,144 @@
+<?php
+  session_start();
+  
+  // Connect to database
+  /**
+   * Use these lines only if the file requires database connection. 
+   */
+	$config = require('config.php');
+	$dsn = $config['connection'] . ';dbname=' . $config['dbname'] . ';charset=' . $config['charset'];
+	try {
+		$pdo = new PDO($dsn, $config['username'], $config['password'], $config['options']);
+	} catch (PDOException $e) {
+		die($e->getMessage());
+  }
+  
+  // Retrieve events and their organizers
+  $sql = 'SELECT event.name, event.date, event.time, user.name AS organizer
+  FROM event
+  INNER JOIN user ON event.user_id = user.id
+  ORDER BY event.date desc';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $events = $stmt->fetchAll();
+
+  // Separate events into upcoming and past
+  $upcomingEvents = [];
+  $pastEvents = [];
+
+  foreach ($events as $event) {
+    // Convert 00:00:00 to 00:00 AM/PM
+    $event['time'] = date('h:i A', strtotime($event['time']));
+
+    // Check whether event's datetime >= today 
+    if (date('Y-m-d H:i:s', strtotime($event['date'] . ' ' . $event['time'])) >= date('Y-m-d H:i:s')) {
+      array_push($upcomingEvents, $event);
+    } else {
+      array_push($pastEvents, $event);
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require('partials/html-head.php'); ?>
+</head>
+<body>
+  <!-- header -->
+  <?php require('partials/header.php'); ?>
+  <br>
+
+  <!-- body -->
+  <div class="container">
+		<div class="row">
+			<?php if (isset($_SESSION['eventAttendanceMessage'])) { ?>
+				<div class="alert alert-success">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<strong>Success!</strong> <?php echo $_SESSION['eventAttendanceMessage']; ?>
+				</div>
+			<?php } ?>
+			<?php unset($_SESSION['eventAttendanceMessage']); ?>
+
+      <div class="col">
+        <ul class="nav nav-tabs nav-stacked">
+          <li class="nav-item">
+            <a href="#upcoming" class="nav-link active" data-toggle="tab">Upcoming Events</a>
+          </li>
+          <li class="nav-item">
+            <a href="#past" class="nav-link" data-toggle="tab">Past Events</a>
+          </li>
+        </ul>
+    
+        <div class="tab-content">
+          <div class="tab-pane active" id="upcoming">
+            <h3>Upcoming Events</h3>
+            <div class="row">
+              <?php foreach ($upcomingEvents as $event) { ?>
+                <div class="col-sm-4 card" data-toggle="modal" data-target="#eventModal">
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $event['name']; ?></h5>
+                    <p>By: <?php echo $event['organizer']; ?></p>
+                    <p>Date: <?php echo $event['date']; ?></p>
+                    <p>Time: <?php echo $event['time']; ?></p>
+                  </div>
+                </div>
+              <?php } ?>
+            </div>
+          </div>
+          <div class="tab-pane" id="past">
+            <h3>Past Events</h3>
+            <div class="row">
+              <?php foreach ($pastEvents as $event) { ?>
+                <div class="col-sm-4 card" data-toggle="modal" data-target="#eventModal">
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $event['name']; ?></h5>
+                    <p>By: <?php echo $event['organizer']; ?></p>
+                    <p>Date: <?php echo $event['date']; ?></p>
+                    <p>Time: <?php echo $event['time']; ?></p>
+                  </div> 
+                </div>
+              <?php } ?>
+            </div>
+          </div>	        
+        </div>		
+      </div>
+		</div>
+	</div>
+
+	<div id="eventModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+          <h4 class="modal-title"></h4>
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+
+				<div class="modal-body">
+					<p>By: <span class="event-organizer"></span></p>
+					<p>Time: <span class="event-time"></span></p>
+					<p>Date: <span class="event-date"></span></p>
+					<p>Venue: <span class="event-venue"></span></p>
+					<p>Description: <span class="event-description"></span></p>
+				</div>
+
+				<div class="modal-footer">
+					<form method="POST" action="api.php">
+						<?php if (isset($_SESSION['username'])) { ?>
+							<input type="hidden" name="eventName" class="input-event-name">
+							<input type="hidden" name="eventAttendance" class="input-attendance">
+							<input type="submit" name="" value="&#10004; Going" class="btn going-btn">
+						<?php } else { ?>
+							<a href="/login.php" class="btn btn-default" role="button">&#10004; Going</a>
+						<?php } ?>
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <!-- footer -->
+  <?php require('partials/footer.php'); ?>
+</body>
+</html>
