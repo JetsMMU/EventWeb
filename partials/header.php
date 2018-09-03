4dfc5fef616284674b4a2e6c7a81a072428386f0
Alfred Loo <alfred97620@gmail.com>
2018-09-04 01:14:59 +0800

    update header content

--- partials/header.php
+++ partials/header.php
@@ -13,12 +13,23 @@
         <li class="nav-item">
           <a class="nav-link" href="/event.php">Browse Events</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Sign Up</a>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="/signup.php">Sign Up</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/login.php">Login</a>
-        </li>
+        </li> -->
+        <?php if(isset($_SESSION['username'])): ?>
+					<li class="nav-item"><a class="nav-link" href="/create-event.php">Create Event</a></li>
+					<li class="nav-item"><a class="nav-link" href="/dashboard.php">Dashboard</a></li>
+					<li class="nav-item"><a class="nav-link" href="/profile.php">My Profile</a><li>
+				<?php endif; ?>
+				<?php if(isset($_SESSION['username'])): ?>
+					<li class="nav-item"><a class="nav-link" href="/logout.php">Logout</a></li>
+				<?php else: ?>
+					<li class="nav-item"><a class="nav-link" href="/signup.php">Sign Up</a></li>
+					<li class="nav-item"><a class="nav-link" href="/login.php">Login</a></li>
+				<?php endif; ?>
         <!-- To add a new file to header -->
         <!-- <li class="nav-item">
           <a class="nav-link" href="filename.php">filename/suitable name</a>
